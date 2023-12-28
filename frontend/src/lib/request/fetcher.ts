9c9f0664988f08661f869a03d40cec05e9889665
ASHISH7865 <jaiswalashish9467@gmail.com>
2023-12-28 23:58:35 +0530

    fixed fetcher function

--- frontend/src/lib/request/fetcher.ts
+++ frontend/src/lib/request/fetcher.ts
@@ -3,7 +3,7 @@ import { FetcherOptions } from "./types";
 
 // const baseUrl: string =
 //   process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://127.0.0.1:1337';
-const baseUrl: string = 'http://127.0.0.1:1337';
+const baseUrl: string = process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337';
 
 export async function fetcher(url: string, options: FetcherOptions = {}) {
   const newUrl = `${baseUrl}/api/${url}`;
