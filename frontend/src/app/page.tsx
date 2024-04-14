1b435a262da3947b2e6600a9473d0fa21e7ec2b0
ASHISH7865 <jaiswalashish9467@gmail.com>
2024-04-14 21:42:44 +0530

    fix dark mode

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Hero from "@/components/PageComponents/Hero/Hero";
 import Navbar from "@/components/PageComponents/Navbar/Navbar";
 import Skills from "@/components/PageComponents/Skills/Skills";
@@ -6,6 +7,8 @@ import Contact from "@/components/PageComponents/Contact/Contact";
 import { Metadata } from "next";
 import { portfolioConstant } from "@/lib/portfolioConstant";
 import { AnimatedFeatureProjects } from "@/components/PageComponents/Projects/AnimatedFeatureProject";
+import { useTheme } from "next-themes";
+import { useEffect } from "react";
 
 const componentMap = {
   Hero: Hero,
@@ -13,12 +16,13 @@ const componentMap = {
   Projects: Projects,
 };
 
-export const metadata: Metadata = {
-  title: "Ashish Portfolio | Home",
-  description: "Ashish Portfolio",
-};
-
 export default async function Home() {
+  const { setTheme } = useTheme();
+
+  useEffect(() => {
+    setTheme("dark");
+  }, []);
+
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <Hero data={portfolioConstant?.herodata} />
